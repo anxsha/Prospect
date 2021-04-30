cca40ca08fd1fe1d8e2c9e2c29770f253aad06ac
anxsha <jakubmolek@protonmail.ch>
2021-04-30 20:26:56 +0200

    Rover position constants can be freely changed.

--- Rover.cpp
+++ Rover.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 
 void Rover::Draw() {
-  constexpr float ORIGIN_Y = -20.0f;
+  constexpr float ORIGIN_Y = 0.0f;
   constexpr float ORIGIN_X = 0.0f;
   constexpr float ROVER_WIDTH = 60.0f;
   constexpr float ROVER_LENGTH = 80.0f;
@@ -20,21 +20,24 @@ void Rover::Draw() {
   // £¹czenia kó³
   glColor3f(0.3, 0.3, 0.4);
   Cylinder::Draw(ORIGIN_X, ORIGIN_Y, 4.0f, ROVER_WIDTH - 4.0f, 2.0f);
-  Cylinder::Draw(ROVER_LENGTH / 2.0f, ORIGIN_Y, 4.0f, ROVER_WIDTH - 4.0f, 2.0f);
-  Cylinder::Draw(ROVER_LENGTH, ORIGIN_Y, 4.0f, ROVER_WIDTH - 4.0f, 2.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH / 2.0f, ORIGIN_Y, 4.0f,
+                 ROVER_WIDTH - 4.0f, 2.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, 4.0f, ROVER_WIDTH - 4.0f,
+                 2.0f);
 
   // £¹czenie œrodkowe na dole
   glPushMatrix();
   glRotatef(90, 0.0, 1.0, 0.0);
-  Cylinder::Draw(-((ROVER_WIDTH / 2) + (4.5 / 2)), ORIGIN_Y, 0.0f, ROVER_LENGTH, 2.0f);
+  Cylinder::Draw(-((ROVER_WIDTH / 2) + (4.5 / 2)), ORIGIN_Y, ORIGIN_X,
+                 ROVER_LENGTH, 2.0f);
   glPopMatrix();
 
   // £¹czenia do platformy
   glPushMatrix();
   glTranslatef(ORIGIN_X, ORIGIN_Y, 4.5f);
-  //glRotatef(360 - 60, 1.0, 0.0, 0.0);
+  glRotatef(360 - 60, 1.0, 0.0, 0.0);
   glRotatef(45, 0.0, 1.0, 0.0);
-  glTranslatef(ORIGIN_X, -ORIGIN_Y, -4.5f);
+  glTranslatef(-ORIGIN_X, -ORIGIN_Y, -4.5f);
   Cylinder::Draw(ORIGIN_X, ORIGIN_Y, 4.5f, 52.0f, 2.0f);
   glPopMatrix();
 
@@ -42,47 +45,48 @@ void Rover::Draw() {
   glTranslatef(ORIGIN_X, ORIGIN_Y, ROVER_WIDTH);
   glRotatef(240, 1.0, 0.0, 0.0);
   glRotatef(45, 0.0, 1.0, 0.0);
-  glTranslatef(ORIGIN_X, -ORIGIN_Y, -ROVER_WIDTH);
+  glTranslatef(-ORIGIN_X, -ORIGIN_Y, -ROVER_WIDTH);
   Cylinder::Draw(ORIGIN_X, ORIGIN_Y, ROVER_WIDTH, 52.0f, 2.0f);
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(ROVER_LENGTH / 2, ORIGIN_Y, 4.5f);
+  glTranslatef(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, 4.5f);
   glRotatef(360 - 60, 1.0, 0.0, 0.0);
-  glTranslatef(-(ROVER_LENGTH / 2), -ORIGIN_Y, -4.5f);
-  Cylinder::Draw(ROVER_LENGTH / 2, ORIGIN_Y, 4.5f, 35.0f, 2.0f);
+  glTranslatef(-(ORIGIN_X + ROVER_LENGTH / 2), -ORIGIN_Y, -4.5f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, 4.5f, 35.0f, 2.0f);
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(ROVER_LENGTH / 2, ORIGIN_Y, ROVER_WIDTH);
+  glTranslatef(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, ROVER_WIDTH);
   glRotatef(240, 1.0, 0.0, 0.0);
-  glTranslatef(-(ROVER_LENGTH / 2), -ORIGIN_Y, -ROVER_WIDTH);
-  Cylinder::Draw(ROVER_LENGTH / 2, ORIGIN_Y, ROVER_WIDTH, 35.0f, 2.0f);
+  glTranslatef(-(ORIGIN_X + ROVER_LENGTH / 2), -ORIGIN_Y, -ROVER_WIDTH);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, ROVER_WIDTH, 35.0f,
+                 2.0f);
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(ROVER_LENGTH, ORIGIN_Y, 4.5f);
+  glTranslatef(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, 4.5f);
   glRotatef(120, 1.0, 0.0, 0.0);
   glRotatef(235, 0.0, 1.0, 0.0);
-  glTranslatef(-(ROVER_LENGTH), -ORIGIN_Y, -4.5f);
-  Cylinder::Draw(ROVER_LENGTH, ORIGIN_Y, 4.5f, 58.0f, 2.0f);
+  glTranslatef(-(ORIGIN_X + ROVER_LENGTH), -ORIGIN_Y, -4.5f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, 4.5f, 58.0f, 2.0f);
   glPopMatrix();
 
   glPushMatrix();
-  glTranslatef(ROVER_LENGTH, ORIGIN_Y, ROVER_WIDTH);
+  glTranslatef(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, ROVER_WIDTH);
   glRotatef(60, 1.0, 0.0, 0.0);
   glRotatef(235, 0.0, 1.0, 0.0);
-  glTranslatef(-(ROVER_LENGTH), -ORIGIN_Y, -ROVER_WIDTH);
-  Cylinder::Draw(ROVER_LENGTH, ORIGIN_Y, ROVER_WIDTH, 58.0f, 2.0f);
+  glTranslatef(-(ORIGIN_X + ROVER_LENGTH), -ORIGIN_Y, -ROVER_WIDTH);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, ROVER_WIDTH, 58.0f, 2.0f);
   glPopMatrix();
 
   // Platforma
   glColor3f(0.69f, 0.77f, 0.87f);
-  Cuboid::Draw(0.0f, ROVER_LENGTH, ORIGIN_Y + 40.0f, 5.0f, 5.0f, 55.0f);
+  Cuboid::Draw(ORIGIN_X, ROVER_LENGTH, ORIGIN_Y + 40.0f, 5.0f, 5.0f, 55.0f);
 
   // Skrzynia pod platform¹
   glColor3f(0.27f, 0.283f, 0.298f);
-  Cuboid::Draw(12.0f, 30.0f, ORIGIN_Y + 25.0f, 15.0f, 15.0f, 35.0f);
+  Cuboid::Draw(ORIGIN_X + 12.0f, 30.0f, ORIGIN_Y + 25.0f, 15.0f, 15.0f, 35.0f);
 
   // Antena
   // S³up
@@ -91,7 +95,7 @@ void Rover::Draw() {
   glTranslatef(15.0f, ORIGIN_Y + 40.0f + 5.0f, ROVER_WIDTH / 2 - 5.0f);
   glRotatef(270, 1.0f, 0.0f, 0.0f);
   glTranslatef(-15.0f, -(ORIGIN_Y + 40.0f + 5.0f), -(ROVER_WIDTH / 2 - 5.0f));
-  Cylinder::Draw(15.0f, ORIGIN_Y + 40.0f + 5.0f, ROVER_WIDTH / 2 - 5.0f, 25.0f, 1.5f);
+  Cylinder::Draw(ORIGIN_X + 15.0f, ORIGIN_Y + 40.0f + 5.0f, ROVER_WIDTH / 2 - 5.0f, 25.0f, 1.5f);
   glPopMatrix();
   // Odbiornik
   glColor3f(0.8, 0.8, 0.85);
@@ -99,30 +103,30 @@ void Rover::Draw() {
   glTranslatef(15.0f, ORIGIN_Y + 40.0f + 5.0f + 25.0f, ROVER_WIDTH / 2 - 5.0f);
   glRotatef(270, 1.0f, 0.0f, 0.0f);
   glTranslatef(-15.0f, -(ORIGIN_Y + 40.0f + 5.0f + 25.0f), -(ROVER_WIDTH / 2 - 5.0f));
-  Cylinder::Draw(15.0f, ORIGIN_Y + 40.0f + 5.0f + 25.0f, ROVER_WIDTH / 2 - 5.0f, 1.0f, 10.0f);
+  Cylinder::Draw(ORIGIN_X + 15.0f, ORIGIN_Y + 40.0f + 5.0f + 25.0f, ROVER_WIDTH / 2 - 5.0f, 1.0f, 10.0f);
   glPopMatrix();
 
   // Czujniki
   glColor3f(0.8f, 0.9f, 0.85f);
-  Cylinder::Draw(20.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -1.5f, 2.5f);
-  Cylinder::Draw(30.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -1.5f, 2.5f);
-  Cylinder::Draw(20.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 1.5f, 2.5f);
-  Cylinder::Draw(30.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 1.5f, 2.5f);
+  Cylinder::Draw(ORIGIN_X + 20.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -1.5f, 2.5f);
+  Cylinder::Draw(ORIGIN_X + 30.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -1.5f, 2.5f);
+  Cylinder::Draw(ORIGIN_X + 20.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 1.5f, 2.5f);
+  Cylinder::Draw(ORIGIN_X + 30.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 1.5f, 2.5f);
   glColor3f(0.1f, 0.1f, 0.1f);
-  Cylinder::Draw(20.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -2.5f, 0.5f);
-  Cylinder::Draw(30.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -2.5f, 0.5f);
-  Cylinder::Draw(20.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 2.5f, 0.5f);
-  Cylinder::Draw(30.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 2.5f, 0.5f);
+  Cylinder::Draw(ORIGIN_X + 20.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -2.5f, 0.5f);
+  Cylinder::Draw(ORIGIN_X + 30.0f, ORIGIN_Y + 25.0f + 5.0f, 15.0f, -2.5f, 0.5f);
+  Cylinder::Draw(ORIGIN_X + 20.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 2.5f, 0.5f);
+  Cylinder::Draw(ORIGIN_X + 30.0f, ORIGIN_Y + 25.0f + 5.0f, 50.0f, 2.5f, 0.5f);
 
   // Jedna strona kó³
   glColor3f(0.3, 0.3, 0.3);
   Cylinder::Draw(ORIGIN_X, ORIGIN_Y, 0.0f, 4.5f, 10.0f);
-  Cylinder::Draw(ROVER_LENGTH / 2, ORIGIN_Y, 0.0f, 4.5f, 10.0f);
-  Cylinder::Draw(ROVER_LENGTH, ORIGIN_Y, 0.0f, 4.5f, 10.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, 0.0f, 4.5f, 10.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, 0.0f, 4.5f, 10.0f);
   // Druga strona kó³
   Cylinder::Draw(ORIGIN_X, ORIGIN_Y, 60.0f, 4.5f, 10.0f);
-  Cylinder::Draw(ROVER_LENGTH / 2, ORIGIN_Y, 60.0f, 4.5f, 10.0f);
-  Cylinder::Draw(ROVER_LENGTH, ORIGIN_Y, 60.0f, 4.5f, 10.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH / 2, ORIGIN_Y, 60.0f, 4.5f, 10.0f);
+  Cylinder::Draw(ORIGIN_X + ROVER_LENGTH, ORIGIN_Y, 60.0f, 4.5f, 10.0f);
 
   glPopMatrix();
 }
